372cc38d1ef4e7a0f45cb8b0a70c58addbcfe5b2
Avery Khoo <averykhoo@gmail.com>
2023-07-05 11:30:51 +0800

    update this

--- simhash.py
+++ simhash.py
@@ -1,35 +1,30 @@
 import hashlib
 from functools import lru_cache
 
-try:
-    (123).bit_count()  # available starting in python 3.10
 
+def hamming_distance(x: int, y: int) -> int:
+    """
+    number of bits that are DIFFERENT between two binary integers
 
-    def hamming_distance(x: int, y: int) -> int:
-        """
-        number of bits that are DIFFERENT between two binary integers
+    :param x: non-negative 64-bit integer
+    :param y: non-negative 64-bit integer
+    :return:
+    """
+    # sanity check
+    assert 0 <= x < 0x1_0000_0000_0000_0000
+    assert 0 <= y < 0x1_0000_0000_0000_0000
 
-        see: https://docs.python.org/3/library/stdtypes.html#int.bit_count
-        for below implementation, see: https://en.wikipedia.org/wiki/Hamming_weight
+    # https://docs.python.org/3/library/stdtypes.html#int.bit_count
+    try:
+        return (x ^ y).bit_count()  # only available from python 3.10 onwards
 
-        :param x:
-        :param y:
-        :return:
-        """
-        return (x ^ y).bit_count()
-except AttributeError:
-
-    def hamming_distance(x, y):
+    # https://en.wikipedia.org/wiki/Hamming_weight
+    except AttributeError:
         res = x ^ y
-        # if res < 0:
-        #     res = - res  # should never happen
-        if 0 <= res < 0x1_0000_0000_0000_0000:
-            res -= (res >> 1) & 0x5555_5555_5555_5555
-            res = (res & 0x3333_3333_3333_3333) + ((res >> 2) & 0x3333_3333_3333_3333)
-            res = (res + (res >> 4)) & 0X0F0F_0F0F_0F0F_0F0F
-            return (res * 0x0101_0101_0101_0101) >> 56
-        else:
-            return bin(res).count('1')
+        res -= (res >> 1) & 0x5555_5555_5555_5555
+        res = (res & 0x3333_3333_3333_3333) + ((res >> 2) & 0x3333_3333_3333_3333)
+        res = (res + (res >> 4)) & 0X0F0F_0F0F_0F0F_0F0F
+        return (res * 0x0101_0101_0101_0101) >> 56
 
 
 @lru_cache(maxsize=0xFFFF)
