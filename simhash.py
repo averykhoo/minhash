83784e87c239f9149928e21a2aeb4cba9a422911
Avery Khoo <averykhoo@gmail.com>
2023-07-05 11:25:20 +0800

    add a bit more code

--- simhash.py
+++ simhash.py
@@ -1,3 +1,6 @@
+import hashlib
+from functools import lru_cache
+
 try:
     (123).bit_count()  # available starting in python 3.10
 
@@ -27,3 +30,32 @@ except AttributeError:
             return (res * 0x0101_0101_0101_0101) >> 56
         else:
             return bin(res).count('1')
+
+
+@lru_cache(maxsize=0xFFFF)
+def hash_string_to_int(text: str) -> int:
+    """
+    consistently hash a string to an integer
+
+    :param text:
+    :return: a 64-bit non-negative integer
+    """
+    return int.from_bytes(hashlib.blake2b(text.encode('utf8'), digest_size=8).digest(), 'big')
+
+
+BITARRAY_LOOKUP = {bytes([byte_value]): [1 if bit_value == '1' else 0 for bit_value in f'{byte_value:08b}']
+                   for byte_value in range(256)}
+
+
+@lru_cache(maxsize=0xFFFF)
+def hash_string_to_bitarray(text: str) -> list[int]:
+    """
+    consistently hash a string to an array of bits
+
+    :param text:
+    :return: a 64-bit non-negative integer
+    """
+    out = []
+    for byte in hashlib.blake2b(text.encode('utf8'), digest_size=8).digest():
+        out.extend(BITARRAY_LOOKUP[byte])
+    return out
