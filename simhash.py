e65dab626d4e54649eb20f022161eb66955a3c68
Avery Khoo <averykhoo@gmail.com>
2023-07-06 11:11:37 +0800

    seems to work

--- simhash.py
+++ simhash.py
@@ -2,6 +2,8 @@ import hashlib
 from functools import lru_cache
 from typing import List
 
+HASH_LENGTH_BYTES = 8  # 64-bit hash
+
 
 def hamming_distance(x: int, y: int) -> int:
     """
@@ -28,15 +30,16 @@ def hamming_distance(x: int, y: int) -> int:
 
     # https://en.wikipedia.org/wiki/Hamming_weight
     except AttributeError:
+        # warning: this code only works for up to uint64
         res = x ^ y
         res -= (res >> 1) & 0x5555_5555_5555_5555
         res = (res & 0x3333_3333_3333_3333) + ((res >> 2) & 0x3333_3333_3333_3333)
         res = (res + (res >> 4)) & 0X0F0F_0F0F_0F0F_0F0F
-        return (res * 0x0101_0101_0101_0101) >> 56
+        return ((res * 0x0101_0101_0101_0101) & 0xFFFF_FFFF_FFFF_FFFF) >> 56
 
 
 @lru_cache(maxsize=0xFFFF)
-def hash_string_to_int(text: str) -> int:
+def hash_token_to_int(text: str) -> int:
     """
     consistently hash a string to an integer
 
@@ -50,9 +53,10 @@ BITARRAY_LOOKUP = [[1 if bit_value == '1' else 0 for bit_value in f'{byte_value:
 
 
 @lru_cache(maxsize=0xFFFF)
-def hash_string_to_bitarray(text: str) -> List[int]:
+def hash_token_to_bitarray(text: str) -> List[int]:
     """
     consistently hash a string to an array of bits
+    warning: assumes big endianness
 
     :param text:
     :return: a 64-bit non-negative integer
@@ -107,7 +111,7 @@ def bitarray_to_integer(bitarray: List[int]) -> int:
     return sum(v << (_constant - i) for i, v in enumerate(bitarray))
 
 
-def integer_to_bitarray(number: int, bit_length: int = 64) -> List[int]:
+def integer_to_bitarray(number: int, *, bit_length: int = HASH_LENGTH_BYTES * 8) -> List[int]:
     """
     >>> integer_to_bitarray(7, 5)
     [0, 0, 1, 1, 1]
@@ -124,3 +128,34 @@ def integer_to_bitarray(number: int, bit_length: int = 64) -> List[int]:
     """
     assert bit_length >= 1
     return [(number >> i) & 1 for i in range(bit_length - 1, -1, -1)]
+
+
+def tokenize(text: str) -> List[str]:
+    return text.split()
+
+
+def simhash(text: str) -> int:
+    """
+    >>> simhash('hello world')
+    18426460281723402111
+    >>> simhash('hello $USER')
+    18444489699396762749
+
+    :param text:
+    :return:
+    """
+    return bitarray_to_integer(simhash_average_bitarrays(*(hash_token_to_bitarray(token) for token in tokenize(text))))
+
+
+def integer_to_hex(number: int, *, length: int = HASH_LENGTH_BYTES) -> str:
+    return number.to_bytes(length, 'big').hex()
+
+
+if __name__ == '__main__':
+    str_x = 'def int2hex(number: int, len: int = 8) -> str:'
+    str_y = 'def integer_to_hex(number: int, *, length: int = HASH_LENGTH_BYTES) -> str:'
+
+    print(bin(simhash(str_x)))
+    print(bin(simhash(str_y)))
+    print(''.join(['^ '[x == y] for x, y in zip(bin(simhash(str_y)), bin(simhash(str_x)))]))
+    print(hamming_distance(simhash(str_x), simhash(str_y)))
